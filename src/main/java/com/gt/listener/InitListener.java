1d03f7512f17c73bb618cf8d17ee542327d91e1b
gutao <gutaoai@126.com>
2015-10-07 22:55:00 +0800

    gutao

---
+++ src/main/java/com/gt/listener/InitListener.java
@@ -0,0 +1,15 @@
+package com.gt.listener;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.web.context.ServletContextAware;
+
+public class InitListener implements ServletContextAware {
+
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/com/gt/services/Impl/BaseServiceImpl.java
+++ src/main/java/com/gt/services/Impl/BaseServiceImpl.java
@@ -14,39 +14,38 @@ import com.gt.services.base.BaseServiceI;
 public class BaseServiceImpl<T> implements BaseServiceI<T> {
     private Class clazz;
 	public BaseServiceImpl(){
-		/*System.out.println("this代表的是当前调用构造方法的对象："+this);
-		System.out.println("获取当前this对象的父类："+this.getClass().getSuperclass());
-		System.out.println("获取当前this对象父类的信息（包括泛型）:"+this.getClass().getGenericSuperclass());
-		System.out.println();*/
+		/*
+		 * System.out.println("this代表的是当前调用构造方法的对象："+this);
+		   System.out.println("获取当前this对象的父类："+this.getClass().getSuperclass());
+		   System.out.println("获取当前this对象父类的信息（包括泛型）:"+this.getClass().getGenericSuperclass());
+		   System.out.println();
+		 */
 		ParameterizedType type =(ParameterizedType)this.getClass().getGenericSuperclass();
 		clazz = (Class) type.getActualTypeArguments()[0];
 		
 	}
-	@Autowired
-	protected BaseDaoImpl<T> baseDao;
-	@Override
-	public T get() {
-		// TODO Auto-generated method stub
-		T t = baseDao.get(clazz, 1);
-		return t;
-	}
-	@Override
-	public void save(T t) {
-		baseDao.save(t);
-	}
-	@Override
-	public List<T> query() {
-		return baseDao.query();
-	}
-	@Override
-	public void delete(int id) {
-		baseDao.delete(id);
-	}
-	@Override
-	public void update(T t) {
-		baseDao.update(t);
-	}
-
-	
-
+		@Autowired
+		protected BaseDaoImpl<T> baseDao;
+		@Override
+		public T get() {
+			// TODO Auto-generated method stub
+			T t = baseDao.get(clazz, 1);
+			return t;
+		}
+		@Override
+		public void save(T t) {
+			baseDao.save(t);
+		}
+		@Override
+		public List<T> query() {
+			return baseDao.query();
+		}
+		@Override
+		public void delete(int id) {
+			baseDao.delete(id);
+		}
+		@Override
+		public void update(T t) {
+			baseDao.update(t);
+		}
 }
