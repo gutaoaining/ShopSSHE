f9f2ea0a81c7195b1c345bca8b939ec039e566ee
gutao <gutaoai@126.com>
2015-10-24 23:22:57 +0800

    gutao

--- src/main/java/com/gt/listener/InitListener.java
+++ src/main/java/com/gt/listener/InitListener.java
@@ -18,12 +18,12 @@ public class InitListener implements ServletContextListener {
 	ApplicationContext applicationContext = null;
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		//解决方案一:通过直接拿配置文件来获得service,但是并不好，因为加载了两次配置文件
+//		解决方案一:通过直接拿配置文件来获得service,但是并不好，因为加载了两次配置文件
 //		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml","classpath:spring-hibernate.xml");
 //      productService = (ProductServiceI)applicationContext.getBean("productService");
 //      System.out.println("-------------自己的监听器："+productService);
 	    //解决方案二:项目在启动时把spring配置文件通过配置加载，存储了servletContext,只需要获取就可以了
-		  //这里key值有点长，不方便
+		//这里key值有点长，不方便
 //		ApplicationContext applicationContext =(ApplicationContext)sce.getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
 //		productService = (ProductServiceI)applicationContext.getBean("productService");
 //		System.out.println("---------productService:"+productService);
@@ -31,13 +31,11 @@ public class InitListener implements ServletContextListener {
 		applicationContext =(ApplicationContext)WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext());
 		productTimerTask = (ProductTimerTask)applicationContext.getBean("productTimerTask");
 	    productTimerTask.setServletContext(sce.getServletContext());
-		new Timer(true).schedule(productTimerTask, 0, 1000*60*60);
-	    
+		new Timer(true).schedule(productTimerTask, 0, 1000*60*60); 
 	}
-
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-
+		
 	}
 
 }
